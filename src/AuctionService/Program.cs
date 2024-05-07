8c844786afd3f499526c7c170186a12c1e7ca9fd
Pranav Kumar <prnvkmr954@gmail.com>
2024-05-07 21:08:49 +0530

    Added Get And Get By ID API Request for car auction

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -8,8 +8,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt => {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
